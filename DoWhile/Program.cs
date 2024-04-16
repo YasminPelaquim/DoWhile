8a2a6df89750c99bb07b40a5b7ddd88f3981485e
Usuario <Usuario@CPD02-M06>
2024-04-16 10:41:44 -0300

    Adicionar arquivos de projeto.

---
+++ DoWhile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp;
+            double valt = 0;
+
+            do
+            {
+                Console.WriteLine("Qual a descrição da despesa? ");
+                string desp = Console.ReadLine();
+
+                Console.WriteLine("Qual o valor? ");
+                double val = double.Parse(Console.ReadLine());
+
+                valt += val;
+
+                Console.WriteLine("Deseja adicionar mais despesas? Sim/Não ");
+                resp = Console.ReadLine();
+
+            } while (resp == "S");
+            {
+                Console.WriteLine("O gasto total foi de " + valt);
+                Console.WriteLine("Quantas pessoas moram na casa? ");
+                int pes = int.Parse(Console.ReadLine());
+
+                double gastom = valt / pes;
+
+                Console.WriteLine("O gasto médio por morador é de " +  gastom);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
